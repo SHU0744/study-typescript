94c0708fa5ca843fc7c1ecfd8e01ab681a382644
SHU0744 <miyavi.senshu@gmail.com>
2023-10-09 06:55:03 +0900

    型について

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
+let str = {} as { bar: string };
+str.bar = "string";
+let foo: number = 123;
+
+const double = (x: number): number | undefined => {
+  if (x > 0) {
+    return;
+  }
+  return x * 2;
+};
+
+const Home = () => {
   return <div className="">test</div>;
-}
+};
+
+export default Home;
