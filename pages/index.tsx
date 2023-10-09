4ae695bf3a63dcc04fc900643aed79fa415477f8
SHU0744 <miyavi.senshu@gmail.com>
2023-10-09 16:04:05 +0900

    プリミティブ型（Bigint symbol除く）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-let str = {} as { bar: string };
-str.bar = "string";
-let foo: number = 123;
+const bool: boolean = true;
+const num: number = 12;
+const str: string = "a";
+const aa: null = null;
+const und: undefined = undefined;
 
 const double = (x: number): number | undefined => {
   if (x > 0) {
