10dc29214c3cc686d9b8a9193cfc26337ddcc74e
SHU0744 <miyavi.senshu@gmail.com>
2023-10-15 07:25:21 +0900

    型

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { string } from "prop-types";
+
 const bool: boolean = true;
 const boo: true = true;
 const num: number = 12;
@@ -9,6 +11,34 @@ const und: undefined = undefined;
 
 const wid: "wid" = "wid";
 let bar = wid;
+
+const nums: number[] = [1, 2];
+const nums2: Array<number> = [2, 3];
+const nums3: (number | string)[] = [1, "2"];
+const nums4: Array<number | string> = ["a"];
+
+const array1: [number, string] = [1, "a"];
+const array2: any = "a";
+const array3: unknown = "a";
+if (typeof array3 === "string") {
+  array3.substr(2);
+}
+
+const foo = (): void => {
+  alert("a");
+};
+
+const foo2 = (bar: "a" | "b") => {
+  switch (bar) {
+    case "a":
+      return;
+    case "b":
+      return;
+    default:
+      // never
+      break;
+  }
+};
 
 const double = (x: number): number | undefined => {
   if (x > 0) {
