660e0484ba260c7b50ef2ddee08fa08601ffe4e1
SHU0744 <miyavi.senshu@gmail.com>
2023-12-22 12:17:09 +0900

    Todoあぷり

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,73 @@
-import { type } from "os";
-import { string } from "prop-types";
+import { NextPage } from "next";
+import { ChangeEventHandler, VFC, useState } from "react";
 
-const bool: boolean = true;
-const boo: true = true;
-const num: number = 12;
-const numb: 123 = 123;
-const str: string = "a";
-const a: "a" = "a";
-const aa: null = null;
-const und: undefined = undefined;
-
-const wid: "wid" = "wid";
-let bar = wid;
-
-const nums: number[] = [1, 2];
-const nums2: Array<number> = [2, 3];
-const nums3: (number | string)[] = [1, "2"];
-const nums4: Array<number | string> = ["a"];
-
-const array1: [number, string] = [1, "a"];
-const array2: any = "a";
-const array3: unknown = "a";
-if (typeof array3 === "string") {
-  array3.substr(2);
-}
-
-const foo = (): void => {
-  alert("a");
-};
-let obj: { [key: string]: unknown } = {
-  a: true,
-  b: 123,
-};
-let obj2: { [key: string]: { foo: unknown } } = {
-  a: {
-    foo: "a",
-  },
+type Todo = {
+  id: number;
+  label: string;
+  isDone: boolean;
 };
 
-type Foo = {
-  a: string;
-  b: number;
+const Home: NextPage = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const input: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setText(e.target.value);
+  };
+
+  const add = () => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: Math.random(), label: text, isDone: false }];
+    });
+    setText("");
+  };
+  const toggle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === Number(e.target.value)) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+  return (
+    <div>
+      <h1>Todo</h1>
+      <div>
+        <input type="text" value={text} onChange={input} />
+        <button onClick={add}>ボタン</button>
+      </div>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <ListItem todo={todo} toggle={toggle} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
-type Bar = {
-  a: number;
-  c: boolean;
+type ListItemProps = {
+  todo: Todo;
+  toggle: ChangeEventHandler<HTMLInputElement>;
 };
 
-// type FooBar = Foo & Bar;
-type FooBar2 = Foo | Bar;
-
-// const Test: FooBar = {
-//   a: "1",
-//   b: 1,
-//   c: true,
-// };
-
-const test: FooBar2 = {
-  a: "string",
-  b: 1,
-  c: true,
-};
-
-if ("b" in test) {
-  test.a.toString();
-} else {
-  test.a.toFixed();
-}
-
-const foo2 = (bar: "a" | "b") => {
-  switch (bar) {
-    case "a":
-      return;
-    case "b":
-      return;
-    default:
-      // never
-      break;
-  }
-};
-
-const double = (x: number): number | undefined => {
-  if (x > 0) {
-    return;
-  }
-  return x * 2;
-};
-
-const Home = () => {
-  return <div className=""></div>;
+const ListItem: VFC<ListItemProps> = ({ todo, toggle }) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.isDone}
+        onChange={toggle}
+        value={todo.id}
+      />
+      <span>{todo.label}</span>
+    </label>
+  );
 };
 
 export default Home;
